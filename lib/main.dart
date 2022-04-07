6163cc52c0932504030bee08ee0b3840c95a1f07
Isaac Silva <isaackun98@gmail.com>
2022-04-07 14:39:09 -0300

    Starting the weather_app

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
